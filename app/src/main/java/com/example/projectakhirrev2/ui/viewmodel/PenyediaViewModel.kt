d68c0c0024842140b3249b6a1c918d254887b347
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2024-01-07 12:52:54 +0700

    Membuat penyedia view model

---
+++ app/src/main/java/com/example/projectakhirrev2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.projectakhirrev2.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.projectakhirrev2.AplikasiPreOrder
+import com.example.projectakhirrev2.ui.viewmodel.Order.HistoryOrderViewModel
+import com.example.projectakhirrev2.ui.viewmodel.Order.OrderViewModel
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiPreOrder().container.repositoriProduct)
+        }
+        initializer {
+            OrderViewModel(aplikasiPreOrder().container.repositoriPelanggan)
+        }
+        initializer {
+            HistoryOrderViewModel(aplikasiPreOrder().container.repositoriPelanggan)
+        }
+
+    }
+}
+
+fun CreationExtras.aplikasiPreOrder(): AplikasiPreOrder =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiPreOrder)
