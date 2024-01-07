6bde78796af66023c64fdd0a917d51a42ed0e116
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2024-01-07 12:50:58 +0700

    Membuat order view model

---
+++ app/src/main/java/com/example/projectakhirrev2/ui/viewmodel/Order/OrderViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.projectakhirrev2.ui.viewmodel.Order
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.projectakhirrev2.data.pelanggan.Pelanggan
+import com.example.projectakhirrev2.repositori.pelanggan.RepositoriPelanggan
+
+class OrderViewModel(private val repositoriPelanggan: RepositoriPelanggan): ViewModel(){
+    var uiStatePelanggan by mutableStateOf(UIStatePelanggan())
+        private set
+
+    private fun validasiInput(uiState: DetailPelanggan = uiStatePelanggan.detailPelanggan): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && angkatan.isNotBlank() && ukuran.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailPelanggan: DetailPelanggan){
+        uiStatePelanggan =
+            UIStatePelanggan(detailPelanggan = detailPelanggan, isEntryValid = validasiInput(detailPelanggan))
+    }
+
+    suspend fun savePelanggan(){
+        if(validasiInput()){
+            repositoriPelanggan.insertPelanggan(uiStatePelanggan.detailPelanggan.toPelanggan())
+        }
+    }
+}
+data class UIStatePelanggan(
+    val detailPelanggan: DetailPelanggan = DetailPelanggan(),
+    val isEntryValid: Boolean = false
+)
+data class DetailPelanggan(
+    val id: Int = 0,
+    val nama: String = "",
+    val angkatan: String = "",
+    val ukuran: String = "",
+    val telpon: String = "",
+)
+fun DetailPelanggan.toPelanggan(): Pelanggan = Pelanggan(
+    id = id,
+    nama = nama,
+    angkatan = angkatan,
+    ukuran = ukuran,
+    telpon = telpon,
+)
+
+fun Pelanggan.toUiStatePelanggan(isEntryValid: Boolean = false): UIStatePelanggan = UIStatePelanggan(
+    detailPelanggan = this.toDetailPelanggan(),
+    isEntryValid = isEntryValid
+)
+
+fun Pelanggan.toDetailPelanggan(): DetailPelanggan = DetailPelanggan(
+    id = id,
+    nama = nama,
+    angkatan = angkatan,
+    ukuran = ukuran,
+    telpon = telpon,
+)
+
