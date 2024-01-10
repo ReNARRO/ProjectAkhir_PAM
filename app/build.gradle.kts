dbd0de06c85197bde112abda74587b23332388ed
ardianmulya <ardian.mulya7@gmail.com>
2024-01-10 11:51:40 +0700

    menambahkan dependensi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,5 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    implementation("io.coil-kt:coil-compose:2.3.0")
 }
