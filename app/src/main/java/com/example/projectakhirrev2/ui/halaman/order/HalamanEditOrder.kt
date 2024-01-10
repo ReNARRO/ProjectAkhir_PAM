acb822339733912a74a81b56e81bc80b3500d332
ardianmulya <ardian.mulya7@gmail.com>
2024-01-10 13:45:09 +0700

    membuat halaman edit

--- app/src/main/java/com/example/projectakhirrev2/ui/halaman/order/HalamanEditOrder.kt
+++ app/src/main/java/com/example/projectakhirrev2/ui/halaman/order/HalamanEditOrder.kt
@@ -1,2 +1,57 @@
 package com.example.projectakhirrev2.ui.halaman.order
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhirrev2.R
+import com.example.projectakhirrev2.navigasi.DestinasiNavigasi
+import com.example.projectakhirrev2.navigasi.PreOrderAppBar
+import com.example.projectakhirrev2.ui.viewmodel.Order.EditOrderViewModel
+import com.example.projectakhirrev2.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+object ItemEditDestination : DestinasiNavigasi {
+    override val route= "item_edit"
+    override val titleRes= R.string.edit_pelanggan
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditOrderViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            PreOrderAppBar(
+                title = stringResource(ItemEditDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        OrderPelangganBody(
+            uiStatePelanggan = viewModel.pelangganUiState,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePelanggan()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
