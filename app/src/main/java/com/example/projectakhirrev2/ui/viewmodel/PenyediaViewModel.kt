6f2a4ccb2daf338865bf0d30e90430665835a3b7
ardianmulya <ardian.mulya7@gmail.com>
2024-01-10 17:46:03 +0700

    menambahkan DetailOrderViewModel dan EditOrderViewModel

--- app/src/main/java/com/example/projectakhirrev2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/projectakhirrev2/ui/viewmodel/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.projectakhirrev2.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.projectakhirrev2.AplikasiPreOrder
+import com.example.projectakhirrev2.ui.viewmodel.Order.DetailsOrderViewModel
+import com.example.projectakhirrev2.ui.viewmodel.Order.EditOrderViewModel
 import com.example.projectakhirrev2.ui.viewmodel.Order.HistoryOrderViewModel
 import com.example.projectakhirrev2.ui.viewmodel.Order.OrderViewModel
 
@@ -16,6 +19,16 @@ object PenyediaViewModel {
         }
         initializer {
             HistoryOrderViewModel(aplikasiPreOrder().container.repositoriPelanggan)
+        }
+        initializer {
+            DetailsOrderViewModel(createSavedStateHandle(),
+                aplikasiPreOrder().container.repositoriPelanggan
+            )
+        }
+        initializer {
+            EditOrderViewModel(createSavedStateHandle(),
+                aplikasiPreOrder().container.repositoriPelanggan
+            )
         }
 
     }
