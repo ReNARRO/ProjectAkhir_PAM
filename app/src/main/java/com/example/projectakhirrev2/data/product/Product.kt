6399624a3556619dbae711900dc663665eeb0503
ardianmulya <ardian.mulya7@gmail.com>
2024-01-10 11:53:07 +0700

    update code

--- app/src/main/java/com/example/projectakhirrev2/data/product/Product.kt
+++ app/src/main/java/com/example/projectakhirrev2/data/product/Product.kt
@@ -1,14 +1,30 @@
 package com.example.projectakhirrev2.data.product
 
+import android.util.Log
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-@Entity(tableName = "tblProduct")
-data class Product(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val title: String,
-    val price: String,
-    val description: String,
-    val productImageId: String
-)
+object FirestoreUtil {
+    private val db = Firebase.firestore
+
+    fun saveData(name: String, price: String, deskripsi: String, imageUrl: String) {
+        val data = hashMapOf(
+            "name" to name,
+            "price" to price,
+            "deskripsi" to deskripsi,
+            "image_url" to imageUrl
+            // Tambahkan field lain jika diperlukan
+        )
+
+        db.collection("products")
+            .add(data)
+            .addOnSuccessListener { documentReference ->
+                Log.d("FirestoreUtil", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.e("FirestoreUtil", "Error adding document", e)
+            }
+    }
+}
