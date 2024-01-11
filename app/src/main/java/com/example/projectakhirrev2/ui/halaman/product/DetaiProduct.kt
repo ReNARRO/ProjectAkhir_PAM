4c1e37a7fc1b1d4a0650e4adbd738ebea177d86b
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2024-01-11 22:48:33 +0700

    Membetulkan tampilan yang sesuai

--- app/src/main/java/com/example/projectakhirrev2/ui/halaman/product/DetaiProduct.kt
+++ app/src/main/java/com/example/projectakhirrev2/ui/halaman/product/DetaiProduct.kt
@@ -5,24 +5,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -33,7 +25,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,9 +32,8 @@ import coil.compose.rememberImagePainter
 import com.example.projectakhirrev2.R
 import com.example.projectakhirrev2.data.product.Product
 import com.example.projectakhirrev2.navigasi.DestinasiNavigasi
-import com.example.projectakhirrev2.navigasi.PreOrderApp
 import com.example.projectakhirrev2.navigasi.PreOrderAppBar
-import com.example.projectakhirrev2.ui.viewmodel.DetailsPViewModel
+import com.example.projectakhirrev2.ui.viewmodel.Product.DetailsPViewModel
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -212,21 +202,28 @@ fun ItemDetails(
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             ItemDetailsRow(
-                labelResID ="Nama",
+                labelResID = "",
+                itemDetail = product.imageUrl,
+                modifier = modifier.padding(
+                    horizontal = 12.dp
+                ))
+            ItemDetailsRow(
+                labelResID = "Nama Product : ",
                 itemDetail = product.name,
                 modifier = Modifier.padding(
                     horizontal = 12.dp
                 )
             )
             ItemDetailsRow(
-                labelResID = "Alamat",
+                labelResID = "Harga Product : ",
                 itemDetail = product.price,
                 modifier = Modifier.padding(
                     horizontal = 12.dp
                 )
             )
             ItemDetailsRow(
-                labelResID ="No. Telpon",
+                labelResID = "",
+
                 itemDetail = product.deskripsi,
                 modifier = Modifier.padding(
                     horizontal = 12.dp
