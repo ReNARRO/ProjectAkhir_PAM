4f5f34c7741c923cdc340ce17dfaa485b3e7c73b
ardianmulya <ardian.mulya7@gmail.com>
2024-01-11 17:29:26 +0700

    membuat container

--- app/src/main/java/com/example/projectakhirrev2/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/projectakhirrev2/repositori/ContainerApp.kt
@@ -4,13 +4,22 @@ import android.content.Context
 import com.example.projectakhirrev2.data.DatabasePreOrder
 import com.example.projectakhirrev2.repositori.pelanggan.OfflineRepositoriPelanggan
 import com.example.projectakhirrev2.repositori.pelanggan.RepositoriPelanggan
+import com.example.projectakhirrev2.repositori.product.ProductRepo
+import com.example.projectakhirrev2.repositori.product.ProductRepositoryImpl
+import com.google.firebase.firestore.FirebaseFirestore
 
 interface ContainerApp{
     val repositoriPelanggan: RepositoriPelanggan
+    val productRepo : ProductRepo
 }
 class ContainerDataApp(private val context: Context): ContainerApp{
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
     override val repositoriPelanggan: RepositoriPelanggan by lazy {
         OfflineRepositoriPelanggan(DatabasePreOrder.getDatabase(context).pelangganDao())
+    }
+
+    override val productRepo: ProductRepo by lazy {
+        ProductRepositoryImpl(firestore)
     }
 
 }
