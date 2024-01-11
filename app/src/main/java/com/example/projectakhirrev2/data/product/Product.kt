c283521075ac09c1ba5b6ebb6ce0ed962b805f55
ardianmulya <ardian.mulya7@gmail.com>
2024-01-11 17:31:03 +0700

    menambah constructor

--- app/src/main/java/com/example/projectakhirrev2/data/product/Product.kt
+++ app/src/main/java/com/example/projectakhirrev2/data/product/Product.kt
@@ -2,4 +2,6 @@ package com.example.projectakhirrev2.data.product
 
 data class Product(
     val id: String, val name: String, val price: String, val deskripsi: String, val imageUrl: String
-)
+){
+    constructor(): this("","","","","")
+}
